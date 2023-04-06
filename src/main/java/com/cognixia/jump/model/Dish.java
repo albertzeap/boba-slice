e895cc328a6d26fd2535f733494d13bb2842308d
Jesus Ramos <jesusramos.jr1999@gmail.com>
2023-04-06 09:44:32 -0500

    Finished making the initial models

---
+++ src/main/java/com/cognixia/jump/model/Dish.java
@@ -0,0 +1,102 @@
+package com.cognixia.jump.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Dish implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotBlank
+	@Column(nullable = false)
+	private String name;
+	
+	@NotBlank
+	@Column(nullable=false)
+	private String description;
+	
+	@NotBlank
+	@Column(columnDefinition="DEFAULT '1.99'")
+	private Double price;
+	
+	@NotBlank
+	@Column(columnDefinition="DEFAULT 0") // 0 is false
+	private boolean veganFriendly;
+	
+	@ManyToOne
+	@JoinColumn( name = "order_id", referencedColumnName = "id")
+	private Order order;
+
+	public Dish() {
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public boolean isVeganFriendly() {
+		return veganFriendly;
+	}
+
+	public void setVeganFriendly(boolean veganFriendly) {
+		this.veganFriendly = veganFriendly;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public String toString() {
+		return "Dish [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
+				+ ", veganFriendly=" + veganFriendly + ", order=" + order + "]";
+	}
+	
+
+}
--- src/main/java/com/cognixia/jump/model/UserOrder.java
+++ src/main/java/com/cognixia/jump/model/UserOrder.java
@@ -1,5 +1,69 @@
 package com.cognixia.jump.model;
 
-public class UserOrder {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class UserOrder implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn( name = "user_id", referencedColumnName = "id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn( name = "order_id", referencedColumnName = "id")
+	private Order order;
+
+	public UserOrder() {
+
+	}
+
+	public UserOrder(Integer id, User user, Order order) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.order = order;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public String toString() {
+		return "UserOrder [id=" + id + ", user=" + user + ", Order=" + order + "]";
+	}
+	
 }
