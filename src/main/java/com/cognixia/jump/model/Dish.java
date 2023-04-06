5f483f589cecb78c585b8a74e5131789d0800787
Albert <albertzeap@gmail.com>
2023-04-06 11:06:56 -0700

    Integer to String conversion

--- src/main/java/com/cognixia/jump/model/Dish.java
+++ src/main/java/com/cognixia/jump/model/Dish.java
@@ -11,7 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+// import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Dish implements Serializable{
