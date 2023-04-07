aa943f816801e4cfc74d407f8a8e1acce44eef6f
Jesus Ramos <jesusramos.jr1999@gmail.com>
2023-04-07 15:24:26 -0500

    Finished implementing deleteItem for OrderController

--- src/main/java/com/cognixia/jump/controller/OrderController.java
+++ src/main/java/com/cognixia/jump/controller/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognixia.jump.exception.ResourceNotFoundException;
 import com.cognixia.jump.model.Order;
 import com.cognixia.jump.service.OrderService;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -50,15 +52,12 @@ public class OrderController {
     }
     
 
+    @DeleteMapping("/order/{id}")
+    public ResponseEntity<?> removeMenuItem(@PathParam(value="menuItemId") int menuItemId, @PathParam(value="orderId") int orderId) throws ResourceNotFoundException{
 
+        orderService.deleteItem(menuItemId, orderId);
 
-
-    @DeleteMapping("/order{name}")
-    public ResponseEntity<?> removeItem(@PathVariable String name){
-
-        orderService.removeItem(name);
-
-        return ResponseEntity.status(201).body(null);
+        return ResponseEntity.status(200).body("Deleted item with id = " + menuItemId + "from order #" + menuItemId);
     }
     
 }  
